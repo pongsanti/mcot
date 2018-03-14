2c25474ededcb1a2f837574fdd04ebcd7c21b7a7
pongsanti <pongsanti.tanvejsilp@gmail.com>
2018-03-14 07:18:14 +0700

    fix due to update to tesseract 4

--- ocr.sh
+++ ocr.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 fn_ocr() {
-  OCR=$( tesseract $1 -psm 7 stdout tess_config )
+  #OCR=$( tesseract $1 -psm 7 stdout tess_config )
+  OCR=$( tesseract $1 -psm 7 stdout )
+  OCR=$( echo $OCR | sed 's/\o14//g' ) # cut out ^L character
+
   fn_log "Ocr:\t\t${OCR}"
   echo $OCR
 }
