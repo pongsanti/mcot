03ff17eb63a5e6a0b488891d8781423ea6e6d391
pongsanti <pongsanti.tanvejsilp@gmail.com>
2018-03-15 13:57:31 +0700

    log runtime

--- ocr.sh
+++ ocr.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 fn_ocr() {
+  OCR_START=`date +%s`
   #OCR=$( tesseract $1 -psm 7 stdout tess_config )
   OCR=$( tesseract $1 -psm 7 stdout )
   OCR=$( echo $OCR | sed 's/\o14//g' ) # cut out ^L character
 
   fn_log "Ocr:\t\t${OCR}"
+
+  OCR_END=`date +%s`
+  RUNTIME=$(($OCR_END - $OCR_START))
+  fn_log "\t\t\tOCR in seconds:$RUNTIME"
+
   echo $OCR
 }
